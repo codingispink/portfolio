b5cf7f1843fe1bc263b985640d04f3fdd098f948
Katherine Ngo <138828365+codingispink@users.noreply.github.com>
2023-07-26 12:04:46 -0500

    Update color

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -168,11 +168,11 @@ const config = {
 
     // Custom theme
     customTheme: {
-      primary: '#fc055b',
-      secondary: '#219aaf',
-      accent: '#e8d03a',
-      neutral: '#2A2730',
-      'base-100': '#E3E3ED',
+      primary: '#fbd6c6',
+      secondary: '#F8E1E7',
+      accent: '#F7BC3',
+      neutral: '#FFFFFF',
+      'base-100': '#FEFBEF',
       '--rounded-box': '3rem',
       '--rounded-btn': '3rem',
     },
