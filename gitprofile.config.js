5a4a316eafbd9fdb27ca48247386bd8c77fe4e5c
Katherine Ngo <138828365+codingispink@users.noreply.github.com>
2023-07-28 16:06:37 -0500

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -11,7 +11,7 @@ const config = {
     },
   },
   social: {
-    linkedin: 'katherine-ngo-556688159/',
+    linkedin: 'katherine-ngo-556688159',
     twitter: '',
     mastodon: '',
     facebook: '',
